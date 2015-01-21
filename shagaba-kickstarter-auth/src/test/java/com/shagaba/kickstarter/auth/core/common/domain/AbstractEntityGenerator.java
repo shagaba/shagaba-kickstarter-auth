4686bba635701ed2d527b857240cfd58940756d1
shagaba <shai.gabai@shagaba.com>
2015-01-21 23:34:35 +0200

    an abstract entity generator

---
+++ shagaba-kickstarter-auth/src/test/java/com/shagaba/kickstarter/auth/core/common/domain/AbstractEntityGenerator.java
@@ -0,0 +1,25 @@
+package com.shagaba.kickstarter.auth.core.common.domain;
+
+import java.io.Serializable;
+
+import com.shagaba.kickstarter.auth.core.common.util.RandomObjectFiller;
+
+public abstract class AbstractEntityGenerator<T, ID extends Serializable> {
+	
+	public abstract Class<T> entityClass();
+
+	public abstract void update(T entity, String tag);
+	
+	public abstract ID getId(T entity);
+
+	protected T generate() {
+		return RandomObjectFiller.randomObject(entityClass());
+	}
+
+	public T generate(String tag) {
+		T entity = generate();
+		update(entity, tag);
+		return entity;
+	}
+
+}
