104ef654f458d43a909922c4e34437caef04d255
shagaba <shai.gabai@shagaba.com>
2015-01-24 21:59:06 +0200

    rest domains - profile - personal information

---
+++ shagaba-kickstarter-auth/src/main/java/com/shagaba/kickstarter/auth/rest/domain/profile/Address.java
@@ -0,0 +1,309 @@
+package com.shagaba.kickstarter.auth.rest.domain.profile;
+
+public class Address {
+    
+    protected String name;
+
+    protected String firstName;
+
+    protected String lastName;
+
+    protected String companyName;
+
+    protected String addressLine1;
+
+    protected String addressLine2;
+
+    protected String addressLine3;
+
+    protected String city;
+
+    protected String state;
+
+    protected String county;
+
+    protected String country;
+
+    protected String postalCode;
+
+    protected String zip;
+
+    protected Phone phonePrimary;
+
+    protected Phone phoneSecondary;
+
+    protected Phone phoneFax;
+    
+    protected String longitude;
+
+    protected String latitude;
+
+    protected Boolean isDefault;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the companyName
+     */
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    /**
+     * @param companyName the companyName to set
+     */
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    /**
+     * @return the addressLine1
+     */
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    /**
+     * @param addressLine1 the addressLine1 to set
+     */
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    /**
+     * @return the addressLine2
+     */
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    /**
+     * @param addressLine2 the addressLine2 to set
+     */
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    /**
+     * @return the addressLine3
+     */
+    public String getAddressLine3() {
+        return addressLine3;
+    }
+
+    /**
+     * @param addressLine3 the addressLine3 to set
+     */
+    public void setAddressLine3(String addressLine3) {
+        this.addressLine3 = addressLine3;
+    }
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return the state
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * @param state the state to set
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * @return the county
+     */
+    public String getCounty() {
+        return county;
+    }
+
+    /**
+     * @param county the county to set
+     */
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    /**
+     * @return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return the postalCode
+     */
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    /**
+     * @param postalCode the postalCode to set
+     */
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    /**
+     * @return the zip
+     */
+    public String getZip() {
+        return zip;
+    }
+
+    /**
+     * @param zip the zip to set
+     */
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    /**
+     * @return the phonePrimary
+     */
+    public Phone getPhonePrimary() {
+        return phonePrimary;
+    }
+
+    /**
+     * @param phonePrimary the phonePrimary to set
+     */
+    public void setPhonePrimary(Phone phonePrimary) {
+        this.phonePrimary = phonePrimary;
+    }
+
+    /**
+     * @return the phoneSecondary
+     */
+    public Phone getPhoneSecondary() {
+        return phoneSecondary;
+    }
+
+    /**
+     * @param phoneSecondary the phoneSecondary to set
+     */
+    public void setPhoneSecondary(Phone phoneSecondary) {
+        this.phoneSecondary = phoneSecondary;
+    }
+
+    /**
+     * @return the phoneFax
+     */
+    public Phone getPhoneFax() {
+        return phoneFax;
+    }
+
+    /**
+     * @param phoneFax the phoneFax to set
+     */
+    public void setPhoneFax(Phone phoneFax) {
+        this.phoneFax = phoneFax;
+    }
+
+    /**
+     * @return the longitude
+     */
+    public String getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * @param longitude the longitude to set
+     */
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    /**
+     * @return the latitude
+     */
+    public String getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * @param latitude the latitude to set
+     */
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    /**
+     * @return the isDefault
+     */
+    public Boolean getIsDefault() {
+        return isDefault;
+    }
+
+    /**
+     * @param isDefault the isDefault to set
+     */
+    public void setIsDefault(Boolean isDefault) {
+        this.isDefault = isDefault;
+    }
+
+}
