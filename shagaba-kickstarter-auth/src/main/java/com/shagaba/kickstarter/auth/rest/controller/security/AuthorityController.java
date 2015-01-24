cb68b49443aaa826601cc0a2ec57cf63ec190e5e
shagaba <shai.gabai@shagaba.com>
2015-01-24 22:35:35 +0200

    rest controller - authority & role

---
+++ shagaba-kickstarter-auth/src/main/java/com/shagaba/kickstarter/auth/rest/controller/security/AuthorityController.java
@@ -0,0 +1,60 @@
+package com.shagaba.kickstarter.auth.rest.controller.security;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shagaba.kickstarter.auth.common.service.mapper.MappingService;
+import com.shagaba.kickstarter.auth.core.service.security.AuthorityService;
+import com.shagaba.kickstarter.auth.rest.domain.security.Authority;
+
+@RequestMapping(value = "/security/authorities")
+@RestController
+public class AuthorityController {
+    
+    @Resource
+    protected AuthorityService authorityService;
+
+    @Resource
+    protected MappingService mappingService;
+
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Authority create(@Valid @RequestBody Authority authority) {
+        com.shagaba.kickstarter.auth.core.domain.security.Authority coreAuthority = mappingService.map(authority, com.shagaba.kickstarter.auth.core.domain.security.Authority.class);
+        coreAuthority = authorityService.create(coreAuthority);
+        return mappingService.map(coreAuthority, Authority.class);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Authority> getAllAuthorities() {
+        List<com.shagaba.kickstarter.auth.core.domain.security.Authority> coreAuthorities = authorityService.getAllAuthorities();
+        return mappingService.map(coreAuthorities, Authority.class);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Authority getAuthorityById(@PathVariable("id") String id) {
+        com.shagaba.kickstarter.auth.core.domain.security.Authority coreAuthority =  authorityService.getAuthorityById(id);
+        return mappingService.map(coreAuthority, Authority.class);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Authority update(@Valid @RequestBody Authority authority) {
+        com.shagaba.kickstarter.auth.core.domain.security.Authority coreAuthority = mappingService.map(authority, com.shagaba.kickstarter.auth.core.domain.security.Authority.class);
+        coreAuthority = authorityService.update(coreAuthority);
+        return mappingService.map(coreAuthority, Authority.class);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@PathVariable("id") String id) {
+        authorityService.delete(id);
+    }
+    
+}
---
+++ shagaba-kickstarter-auth/src/main/java/com/shagaba/kickstarter/auth/rest/controller/security/RoleController.java
@@ -0,0 +1,62 @@
+package com.shagaba.kickstarter.auth.rest.controller.security;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shagaba.kickstarter.auth.common.service.mapper.MappingService;
+import com.shagaba.kickstarter.auth.core.service.security.RoleService;
+import com.shagaba.kickstarter.auth.rest.domain.security.Role;
+
+@RequestMapping(value = "/security/roles")
+@RestController
+public class RoleController {
+    
+    @Resource
+    protected RoleService roleService;
+
+    @Resource
+    protected MappingService mappingService;
+
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Role create(@Valid @RequestBody Role role) {
+        com.shagaba.kickstarter.auth.core.domain.security.Role coreRole = mappingService.map(role, com.shagaba.kickstarter.auth.core.domain.security.Role.class);
+        coreRole = roleService.create(coreRole);
+        return mappingService.map(coreRole, Role.class);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Role> getAllRoles() {
+        List<com.shagaba.kickstarter.auth.core.domain.security.Role> coreRoles = roleService.getAllRoles();
+        return mappingService.map(coreRoles, Role.class);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Role getRoleById(@PathVariable("id") String id) {
+        com.shagaba.kickstarter.auth.core.domain.security.Role coreRole = roleService.getRoleById(id);
+        return mappingService.map(coreRole, Role.class);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Role update(@Valid @RequestBody Role role) {
+        com.shagaba.kickstarter.auth.core.domain.security.Role coreRole = mappingService.map(role, com.shagaba.kickstarter.auth.core.domain.security.Role.class);
+        coreRole = roleService.update(coreRole);
+        return mappingService.map(coreRole, Role.class);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@PathVariable("id") String id) {
+        roleService.delete(id);
+    }
+
+    
+    
+}
